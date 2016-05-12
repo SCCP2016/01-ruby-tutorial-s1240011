76d9c8ee5283f1736b4ad9d54d269313b3f732f6
s1240011 <s1240011@u-aizu.ac.jp>
2016-05-12 18:55:24 +0900

    if

---
+++ itp1-2.rb
@@ -0,0 +1,16 @@
+a, b = STDIN.gets.split.map(&:to_i)
+
+
+if a <= b then
+ puts ' a < b '
+elsif a > b then
+puts ' a > b '
+else
+puts 'a == b'
+end
+
+if a < b && b< c then
+puts 'Yes'
+else
+puts 'No'
+end
